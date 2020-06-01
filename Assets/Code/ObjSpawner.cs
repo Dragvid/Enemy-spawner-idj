b7bf9af8193723c4fa40e42f41e6e4eab00b08ed
Dragvid <55921056+Dragvid@users.noreply.github.com>
2020-06-01 19:05:52 +0100

    começo do código base

---
+++ Assets/Code/ObjSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjSpawner : MonoBehaviour
+{
+    public float radius;
+    
+    Spawner spawner;
+    void Start()
+    {
+        spawner = Spawner.instance;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        spawner.SpawnFromPool("enemy", transform.position, Quaternion.identity);
+    }
+}
